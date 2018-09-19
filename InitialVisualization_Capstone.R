a0a473bfc730ad8c741ca14d260a3293d15a065d
sitruks <kurtis.santillanes@gmail.com>
2018-09-18 19:56:02 -0700

    updates

--- InitialVisualization_Capstone.R
+++ InitialVisualization_Capstone.R
@@ -3,28 +3,34 @@
 Packages <- c("readxl", "tidyverse", "tidytext")
 lapply(Packages, library, character.only = TRUE)
 
-#how to load multiple csv files in the same folder. assign the file name minus ".csv".
-# is this a loop with lists?
-# I would like to learn this trick since I believe being able to load data as quick as possible is important.
-# files <- list.files(path = "C:/Users/v-kursan/Downloads/Capstone_DS/Data/", pattern = "*.csv")
+#load files
 
+teams <- read.csv("Data/Teams.csv")
+seasons <- read.csv("Data/Seasons.csv")
+seeds <- read.csv("Data/NCAATourneySeeds.csv")
+conferences <- read.csv("Data/Conferences.csv")
+coaches <- read.csv("Data/TeamCoaches.csv")
+team_conferences <- read.csv("Data/TeamConferences.csv")
+tour_compact_res <- read.csv("Data/NCAATourneyCompactResults.csv")
+tour_detail_res <- read.csv("Data/NCAATourneyDetailedResults.csv")
+seas_compact_res <- read.csv("Data/RegularSeasonCompactResults.csv")
+seas_detail_res <- read.csv("Data/RegularSeasonDetailedResults.csv")
+cities <- read_csv("Data/Cities.csv")
+game_cities <- read_csv("Data/GameCities.csv")
+tour_games <- read_csv("Data/ConferenceTourneyGames.csv")
+tour_slots <- read_csv("Data/NCAATourneySlots.csv")
+second_tour_compact_res <- read_csv("Data/SecondaryTourneyCompactResults.csv")
+second_tour_teams <- read_csv("Data/SecondaryTourneyTeams.csv")
+teams_spellings <- read_csv("Data/TeamSpellings.csv")
+venues <- read_csv("Data/Venues_Clean_w_Coords.csv")
 
-Cities <- read_csv("Data/Cities.csv")
-Conferences <- read_csv("Data/Conferences.csv")
-ConferenceTourneyGames <- read_csv("Data/ConferenceTourneyGames.csv")
-GameCities <- read_csv("Data/GameCities.csv")
-NCAATourneyCompactResults <- read_csv("Data/NCAATourneyCompactResults.csv")
-NCAATourneyDetailedResults <- read_csv("Data/NCAATourneyDetailedResults.csv")
-NCAATourneySeeds <- read_csv("Data/NCAATourneySeeds.csv")
-NCAATourneySlots <- read_csv("Data/NCAATourneySlots.csv")
-#RegularSeasonCompactResults <- read_csv("Data/RegularSeasonCompactResults.csv")
-#RegularSeasonDetailedResults <- read_csv("Data/RegularSeasonDetailedResults.csv")
-Seasons <- read_csv("Data/Seasons.csv")
-SecondaryTourneyCompactResults <- read_csv("Data/SecondaryTourneyCompactResults.csv")
-SecondaryTourneyTeams <- read_csv("Data/SecondaryTourneyTeams.csv")
-TeamCoaches <- read_csv("Data/TeamCoaches.csv")
-TeamConferences <- read_csv("Data/TeamConferences.csv")
-Teams <- read_csv("Data/Teams.csv")
-TeamSpellings <- read_csv("Data/TeamSpellings.csv")
-TourneyVenues <- read_csv("Data/TourneyVenues.csv")
+#combine files into one list, NCAA_Data, to ease exploratory data
+NCAA_Data <- c(teams, seasons, seeds, conferences, coaches, team_conferences, tour_compact_res, tour_detail_res, seas_compact_res, seas_detail_res, cities, game_cities, tour_games, tour_slots ,second_tour_compact_res, second_tour_teams, teams_spellings, venues)
+#NCAA_Data2 <- c("teams", "seasons", "seeds", "conferences", "coaches", "team_conferences", "tour_compact_res", "tour_detail_res", "seas_compact_res", "seas_detail_res", "cities", "game_cities", "tour_games", "tour_slots" ,"second_tour_compact_res", "second_tour_teams", "teams_spellings", "venues")
+
+lapply(NCAA_Data, str)
+lapply(NCAA_Data, head) #every column
+#lapply(NCAA_Data, unique) #every column detail... 
+
+NCAA_Model <- c(teams, seasons, seeds, conferences, coaches, team_conferences, tour_compact_res, tour_detail_res, seas_compact_res, seas_detail_res, cities, game_cities, tour_games, tour_slots, teams_spellings, venues)
 
